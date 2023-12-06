7edb55761318d374ef27933762351932c8aa4772
ommeleven <dangeom94@gmail.com>
2023-12-06 17:41:34 -0600

    finished creating sidebar

--- soundflow/components/Sidebar.tsx
+++ soundflow/components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -67,9 +68,12 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
         </Box>
         
         <Box className="overflow-y-auto h-full">
-            Song Library
+            <Library/>
         </Box>
     </div>
+    <main className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+    </main>
 </div>
     );
 }
