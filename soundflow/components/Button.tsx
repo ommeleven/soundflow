8f7a448f6d437d528f13c83b6469ca6f0455e686
ommeleven <dangeom94@gmail.com>
2023-12-13 23:57:14 -0600

    fixed  button

--- soundflow/components/Button.tsx
+++ soundflow/components/Button.tsx
@@ -1,38 +1,47 @@
-import React, { forwardRef, ButtonHTMLAttributes } from "react";
-// import clsx from "clsx";
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, disabled, type = "button", ...props }, ref) => {
-    return (
-      <button
-        type={type}
-        disabled={disabled}
-        className={clsx(
-          "w-full",
-          "rounded-full",
-          "bg-green-500",
-          "text-black",
-          "border",
-          "border-transparent",
-          "px-3",
-          "py-3",
-          "disabled:opacity-50",
-          "disabled:cursor-not-allowed",
-          "font-bold",
-          "hover:opacity-75",
-          "transition",
-          className
-        )}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </button>
-    );
-  }
-);
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+  className,
+  children,
+  disabled,
+  type = 'button',
+  ...props
+}, ref) => {
+  return (
+    <button
+      type={type}
+      className={twMerge(
+        `
+        w-full 
+        rounded-full 
+        bg-green-500
+        border
+        border-transparent
+        px-3 
+        py-3 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50
+        text-black
+        font-bold
+        hover:opacity-75
+        transition
+      `,
+        disabled && 'opacity-75 cursor-not-allowed',
+        className
+      )}
+      disabled={disabled}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+});
 
 Button.displayName = "Button";
+
 export default Button;
