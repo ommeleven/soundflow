e3162a49d7ab1226a4dab5c80c419d82fa262b35
ommeleven <dangeom94@gmail.com>
2023-12-14 23:30:23 -0600

    finshed layout

--- soundflow/components/ListItem.tsx
+++ soundflow/components/ListItem.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import Image  from "next/image"
-import { FaPlay } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa"; 
 
 interface ListItemProps {
     image: string;
@@ -55,8 +55,23 @@ const ListItem: React.FC<ListItemProps> = (
                 {name}             
             </p>
             <div
-                className="">
-                    <FaPlay></FaPlay>
+            onClick={onClick}
+                className="
+                    absolute
+                    transition
+                    opacity-0
+                    rounded-full
+                    flex
+                    items-center
+                    bg-green-500
+                    p-4
+                    drop-shadow-md
+                    right-5
+                    group-hover:opacity-100
+                    hover:scale-110
+                    "
+                >
+                    <FaPlay className="text-black"></FaPlay>
             </div>
         </button>
     );
