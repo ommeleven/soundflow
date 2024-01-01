525e7672da2ed114c3f45a3a88a2682819886e85
ommeleven <dangeom94@gmail.com>
2023-12-31 19:59:58 -0600

    fixed authModal

--- soundflow/components/Header.tsx
+++ soundflow/components/Header.tsx
@@ -8,6 +8,8 @@ import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -22,9 +24,16 @@ const  Header: React.FC<HeaderProps> = ({
     const router = useRouter();
 
     const supabaseClient = useSupabaseClient();
+    const { user }  = useUser();
 
-    const handleLogout = () => {
-        // Handle logout later
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+        // TODO: reset any playing songs
+        router.refresh();
+
+        if (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -110,6 +119,23 @@ const  Header: React.FC<HeaderProps> = ({
                 items-center
                 gap-x-4
                 ">
+                    {user ? (
+                        <div
+                            className="flex gap-x-4 items-center">
+                                <Button
+                                    onClick={handleLogout}
+                                    className="bg-white px-6 py-2"
+                                >
+                                    Logout
+                                </Button>
+                                <Button
+                                    onClick = {() => router.push('/account')}
+                                    className="bg-white"    
+                                >
+                                    <FaUserAlt />
+                                </Button>
+                            </div>
+                    ): (
                     <>
                     <div>
                         <Button
@@ -133,7 +159,9 @@ const  Header: React.FC<HeaderProps> = ({
                             Login 
                         </Button>
                     </div>
+                    
                     </>
+                    )}
             </div>
             </div>
            {children}
--- soundflow/components/authModal.tsx
+++ soundflow/components/authModal.tsx
@@ -1,64 +1,64 @@
 "use client";
 
-import { useSupabaseClient,
-        useSessionContext 
-} from "@supabase/auth-helpers-react";
+import React, { useEffect } from 'react';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { 
+  useSessionContext, 
+  useSupabaseClient
+} from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/navigation';
 
-import { useRouter } from "next/navigation";
-import Modal from "./Modal";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeMinimal, ThemeSupa } from "@supabase/auth-ui-shared";
 import useAuthModal from "@/hooks/useAuthModal";
-import { useEffect } from "react";
+
+import Modal from './Modal';
 
 const AuthModal = () => {
-    const supabaseClient = useSupabaseClient();
-    const router = useRouter();
-    const { session } = useSessionContext();
-    const { onClose, isOpen} = useAuthModal();
+  const { session } = useSessionContext();
+  const router = useRouter();
+  const { onClose, isOpen } = useAuthModal();
+  
+  const supabaseClient = useSupabaseClient();
 
-    useEffect(() => {
-        if (session) {
-            router.refresh();
-            onClose();
-        }
-    }, [session, router, onClose]);
-
-    const onChange = (open: boolean) => {
-        if (!open) {
-            onClose();
-        }
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onClose();
     }
-    return  (
-        <Modal
-            title="welcome back"
-            description="login to your account"
-            isOpen
-            onChange = {onChange}    
-        >
-            <Auth
-                theme="dark"
-                magicLink
-                providers={['google', 'facebook']}
-                supabaseClient={supabaseClient}
-                appearance={{
-                    theme: ThemeSupa,
-                    variables: {
-                        default: {
-                            colors: {
-                                brand: '#404040',
-                                brandAccent: '#22c55e'
-                            }
-                        }
-                    }
-                }}
-            />
-        </Modal>
-        
+  }, [session, router, onClose]);
 
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  }
 
-    );
+  return (
+    <Modal 
+      title="Welcome back" 
+      description="Login to your account." 
+      isOpen={isOpen} 
+      onChange={onChange} 
+    >
+      <Auth
+        supabaseClient={supabaseClient}
+        providers={['github']}
+        magicLink={true}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: '#404040',
+                brandAccent: '#22c55e'
+              }
+            }
+          }
+        }}
+        theme="dark"
+      />
+    </Modal>
+  );
 }
-
 
 export default AuthModal;
