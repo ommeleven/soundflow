ec995243178fa6a111b92bc10d162a4df6f007e0
ommeleven <dangeom94@gmail.com>
2023-12-31 20:11:35 -0600

    finished setting up auth functionality

--- soundflow/app/layout.tsx
+++ soundflow/app/layout.tsx
@@ -5,6 +5,7 @@ import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/userProvider'
 import ModalProvider from '@/providers/modalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
         <UserProvider>
           <ModalProvider />
